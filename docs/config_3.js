aa6a8c6509d7339766c0e3a16ea5d9ed73ec2981
Josch Bockler <9265647+jbockler@users.noreply.github.com>
2024-09-04 12:28:43 +0200

    Add entries of group 3 (#5)

--- docs/config_3.js
+++ docs/config_3.js
@@ -1,4 +1,124 @@
 window.config3 = {
     "date": "2024.06",
-    "entries": []
+    "entries": [
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "redis",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "min.io",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Ruby",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_TRIAL,
+            "label": "Rust",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "JavaScript",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "TypeScript",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_HOLD,
+            "label": "jQuery",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Ruby on Rails",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "Elasticsearch",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Openapi (Swagger)",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_TOP_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "Resque",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_TRIAL,
+            "label": "Sidekiq",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "docker",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "HAProxy",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_ADOPT,
+            "label": "cucumber",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_LEFT,
+            "ring": window.RING_ADOPT,
+            "label": "Puma",
+            "active": true,
+            "moved": 0
+        },
+        {
+            "quadrant": window.QUADRANT_BOTTOM_RIGHT,
+            "ring": window.RING_HOLD,
+            "label": "webpacker",
+            "active": true,
+            "moved": 0
+        },
+    ]
 }
