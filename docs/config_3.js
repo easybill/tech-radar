4131f1339101e3bfcbb0df979f0226f330ab8aa0
derklaro <pasqual.koschmieder@easybill.de>
2024-10-10 12:48:15 +0200

    improve ring configurations

--- docs/config_3.js
+++ docs/config_3.js
@@ -1,11 +1,11 @@
 window.config3 = {
     "date": "2024.09",
     "entries": [
-
         {
             "quadrant": window.QUADRANT_BOTTOM_RIGHT,
             "ring": window.RING_WORKS,
             "label": "Rust",
+            "link": "https://rust-lang.org",
             "active": true,
             "moved": 0
         },
@@ -13,6 +13,7 @@ window.config3 = {
             "quadrant": window.QUADRANT_BOTTOM_RIGHT,
             "ring": window.RING_ADOPT,
             "label": "Openapi (Swagger)",
+            "link": "https://swagger.io",
             "active": true,
             "moved": 0
         },
@@ -20,6 +21,7 @@ window.config3 = {
             "quadrant": window.QUADRANT_BOTTOM_LEFT,
             "ring": window.RING_ADOPTED_BUT_BAD,
             "label": "Sidekiq",
+            "link": "https://sidekiq.org",
             "active": true,
             "moved": 0
         },
@@ -27,6 +29,7 @@ window.config3 = {
             "quadrant": window.QUADRANT_BOTTOM_LEFT,
             "ring": window.RING_ADOPT,
             "label": "HAProxy",
+            "link": "https://haproxy.org",
             "active": true,
             "moved": 0
         },
@@ -34,7 +37,7 @@ window.config3 = {
             "quadrant": window.QUADRANT_BOTTOM_RIGHT,
             "ring": window.RING_ADOPT,
             "label": "Cucumber",
-            "link": "https://cucumber.io/",
+            "link": "https://cucumber.io",
             "active": true,
             "moved": 0
         },
@@ -42,6 +45,7 @@ window.config3 = {
             "quadrant": window.QUADRANT_BOTTOM_RIGHT,
             "ring": window.RING_HOLD,
             "label": "webpacker",
+            "link": "https://webpack.js.org",
             "active": true,
             "moved": 0
         },
